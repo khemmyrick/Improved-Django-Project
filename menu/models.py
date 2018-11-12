e726e6c3feeef30fb2e946d7a71c3e8334b63be3
Khem Myrick <khem.myrick@gmail.com>
2018-11-11 22:56:25 -0800

    Updated templatetags.

--- menu/models.py
+++ menu/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Menu(models.Model):
-    season = models.CharField(max_length=20)
+    season = models.CharField(max_length=20)  # unique=True ?
     items = models.ManyToManyField('Item', related_name='items')
     created_date = models.DateTimeField(
         default=timezone.now
